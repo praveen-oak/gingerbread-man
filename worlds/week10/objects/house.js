ae28e81d4d813326b0793036912c701de80355ee
jlliu <jackieleeliu@gmail.com>
2019-12-09 16:55:37 -0500

    adding house obj file

---
+++ worlds/week10/objects/house.js
@@ -0,0 +1,54 @@
+var house = `# Object Export From Tinkercad Server 2015
+
+mtllib obj.mtl
+
+o obj_0
+v -10 		16.5 		13.5
+v -10 		6.5 		13.5
+v 5 		6.5 		13.5
+v 5 		16.5 		13.5
+v 5 		16.5 		3.5
+v 5 		6.5 		3.5
+v -10 		6.5 		3.5
+v -10 		16.5 		3.5
+v -10 		16.5 		14.5
+v -10 		23.5 		8.5
+v -10 		16.5 		2.5
+v 5 		16.5 		2.5
+v 5 		23.5 		8.5
+v 5 		16.5 		14.5
+# 14 vertices
+
+g group_0_undefined
+
+usemtl color_undefined
+s 0
+
+f 1 	2 	3
+f 1 	3 	4
+f 5 	6 	7
+f 5 	7 	8
+f 4 	3 	6
+f 4 	6 	5
+f 11 	8 	10
+f 8 	1 	10
+f 9 	10 	1
+f 12 	5 	8
+f 12 	8 	11
+f 3 	2 	7
+f 3 	7 	6
+f 8 	7 	2
+f 8 	2 	1
+f 10 	13 	12
+f 10 	12 	11
+f 10 	9 	14
+f 10 	14 	13
+f 4 	14 	9
+f 4 	9 	1
+f 13 	14 	4
+f 13 	4 	5
+f 13 	5 	12
+# 24 faces
+
+ #end of obj_0`
+
